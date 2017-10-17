52b7c69b214d40aff4db0c0b983a221f2edfb847
txurtxil <jubadomi@hotmail.com>
2017-10-17 10:36:34 +0200

    shell menu v0.1

---
+++ shellmenu/salon.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+# This script is used to manage SmartHomeBox shell
+# Write: surfero75
+# Date:  2017-06-28
+
+MENUSTR="Selecciona opcion:"
+
+#El tamano de la ventana viene definido por 20 50 8
+OPTION=$(whiptail --title "Panel administración SmartHomeBox - surfero75" \
+    --menu "$MENUSTR" 20 50 8 --cancel-button Finish --ok-button Select \
+    "0"   "Encender Lampara 1" \
+    "1"   "Apagar Lampara 1" \
+    "2"   "Encender Lampara 2" \
+    "3"   "Apagar Lampara 2" \
+    3>&1 1>&2 2>&3)
+
+#Bucle principal, insertar las llamadas a los scripts
+if [ $OPTION = "0" ]; then
+    # Luces Salon
+       /usr/bin/curl "http://192.168.1.10x:8080/json.htm?type=command&param=switchlight&idx=19&switchcmd=On"
+    elif [ $OPTION = "1" ]; then
+       /usr/bin/curl "http://192.168.1.10x:8080/json.htm?type=command&param=switchlight&idx=19&switchcmd=Off"
+    elif [ $OPTION = "2" ]; then
+        /usr/bin/curl "http://192.168.1.10x:8080/json.htm?type=command&param=switchlight&idx=19&switchcmd=On"
+    elif [ $OPTION = "3" ]; then
+        /usr/bin/curl "http://192.168.1.10x:8080/json.htm?type=command&param=switchlight&idx=19&switchcmd=Off"
+else
+        echo "Bad Operation"
+fi
+
+
